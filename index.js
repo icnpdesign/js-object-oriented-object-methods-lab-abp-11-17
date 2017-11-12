68579af53d3ff7094bf1b2f2f2bf6b704023bd37
icnpdesign <inp@bu.edu>
2017-11-12 19:42:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@
       function () {
         return ("You will see great things from Scuber.")
       }
-    this.sayHi = 
-      function () {
+    this.sayHi =
+      function (name, homeState, training) {
         return (`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
       }
   }
