1e4b431fa49dbb0fded8e83c6f6c4ed67f6bafa6
icnpdesign <inp@bu.edu>
2017-11-12 19:55:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-function boardMembers () {
-  let polishedBoardMember;
+class boardMembers {
 
   function BoardMember (name, homeState, training) {
     this.name = name
@@ -7,26 +6,23 @@ function boardMembers () {
     this.training = training
   }
 
-  this = new BoardMember
-
-  this.veto =
-    function () {
+  veto() {
       return ("No, I must disagree")
-    }
-  this.approve =
-    function () {
+  }
+  
+  approve() {
       return ("You can do that!")
-    }
-  this.doCharity =
-    function () {
+  }
+
+  doCharity () {
       return ("I like to help people.")
-    }
-  this.releasePressStatement =
-    function () {
+  }
+
+  releasePressStatement() {
       return ("You will see great things from Scuber.")
-    }
-  this.sayHi =
-    function () {
+  }
+
+  sayHi () {
       return (`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
     }
 }
