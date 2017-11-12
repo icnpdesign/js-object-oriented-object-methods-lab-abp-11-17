dd2da0304732ce36203ea31eab580401554c011c
icnpdesign <inp@bu.edu>
2017-11-12 19:20:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function boardMembers (name, homeState, training) {
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
   let polishedBoardMember =`${name}, ${homeState},${training}`;
 }
 
 function BoardMember (name, homeState, training) {
-  this.name = name;
-  this.homeState = homeState;
-  this.training = training;
+  
   this.veto = function () {
     console.log("No, I must disagree")}
   this.approve = function () {
