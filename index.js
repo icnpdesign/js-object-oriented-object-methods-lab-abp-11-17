87883078d030dbefa5db604dffbb8ffdeea5bec3
icnpdesign <inp@bu.edu>
2017-11-12 19:26:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     this.name = name;
     this.homeState = homeState;
     this.training = training;
-    
+
     this.veto = function () {
       return ("No, I must disagree")}
     this.approve = function () {
@@ -12,7 +12,6 @@
     this.releasePressStatement = function () {
       return ("You will see great things from Scuber.")}
     this.sayHi = function () {
-      return (`Hi, my name is ${name}. I am from ${homestate}, and I was trained in ${training}.`
+      return (`Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
       }
   }
-
